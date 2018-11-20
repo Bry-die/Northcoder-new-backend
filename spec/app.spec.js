89d971bbca87f3d48e0f076f4b313dedbc415fb8
Bry-die <brydie.caine1@gmail.com>
2018-11-20 17:03:03 +0000

    first day

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,8 +2,20 @@ process.env.NODE_ENV = "test";
 const { expect } = require("chai");
 const app = require("../app");
 const request = require("supertest")(app);
+const db = require('../db/connection')
 
 describe('/', () => {
+    beforeEach(() => {
+        return db.migrate
+          .rollback()
+          .then(() => {
+              return db.migrate.latest();
+          })
+          .then(() => {
+              return db.seed.run();
+          })
+          .then(() => console.log('seed success...'));
+    });
     describe('/api', () => {
         describe('/topics', () => {
             it('GET returns staus 200 and an array of topics', () => {
